f37ce86654b29ca57c3f06d749d7101a0d505ded
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2019-10-20 23:23:32 -0300

    Test Category

--- database/factories/GenreFactory.php
+++ database/factories/GenreFactory.php
@@ -2,10 +2,10 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Models\Gender;
+use App\Models\Genre;
 use Faker\Generator as Faker;
 
-$factory->define(Gender::class, function (Faker $faker) {
+$factory->define(Genre::class, function (Faker $faker) {
     return [
       'name' => $faker->colorName,
     ];
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(CategoriesTableSeeder::class);
-        $this->call(GendersTableSeeder::class);
+        $this->call(GenresTableSeeder::class);
     }
 }
