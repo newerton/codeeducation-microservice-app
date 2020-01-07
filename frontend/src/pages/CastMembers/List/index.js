3a402f347ab181c14901ecf8c26a0dadf89fb48d
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2020-01-07 11:26:49 -0300

    Updated categories list

---
+++ frontend/src/pages/CastMembers/List/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from '../Table';
+
+// import { Container } from './styles';
+
+export default function List() {
+  return (
+    <>
+      <h1>Listagem de membros</h1>
+      <Table />
+    </>
+  );
+}
--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Dashboard from '../pages/Dashboard';
 import CategoriesList from '../pages/Categories/List';
+import CastMembersList from '../pages/CastMembers/List';
+import GenresList from '../pages/Genres/List';
 import { DashboardRounded, CategoryRounded } from '@material-ui/icons';
 
 const routes = [
