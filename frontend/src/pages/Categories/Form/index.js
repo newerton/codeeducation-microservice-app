28cd58e75eab21648c19e6441aa645b93b6e9ccb
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2020-01-08 17:55:59 -0300

    Updated categories form

--- frontend/src/pages/Categories/Form/index.js
+++ frontend/src/pages/Categories/Form/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
-import { TextField, Button, Switch, makeStyles } from '@material-ui/core';
+import {
+  TextField,
+  Button,
+  Switch,
+  makeStyles,
+  FormControlLabel,
+} from '@material-ui/core';
 import { Form as Formm, Input } from '@rocketseat/unform';
 // import { Container } from './styles';
 
@@ -14,14 +20,20 @@ const useStyles = makeStyles(theme => {
 
 export default function Form() {
   const classes = useStyles();
+  const inputRef = useRef(false);
   const buttonProps = {
     className: classes.submit,
     variant: 'outlined',
   };
+  const [isActive, setIsActive] = useState();
 
   function handleSubmit(data) {
     console.log(data);
   }
+
+  const handleIsActive = name => event => {
+    setIsActive(!isActive);
+  };
   return (
     <>
       <h1>Adicionar uma nova categoria</h1>
@@ -33,20 +45,31 @@ export default function Form() {
           variant="outlined"
           InputProps={{
             inputComponent: Input,
+            inputProps: { inputRef: { ref: node => console.log('1', node) } },
           }}
           fullWidth
         />
         <TextField
-          id="description"
           label="Descrição"
+          id="description"
+          name="description"
           multiline
           fullWidth
           rows="4"
           variant="outlined"
           margin="normal"
-          InputProps={{
-            inputComponent: Input,
-          }}
+        />
+        <FormControlLabel
+          control={
+            <Switch
+              checked={isActive}
+              onChange={handleIsActive('is_active')}
+              color="primary"
+              id="is_active"
+              name="is_active"
+            />
+          }
+          label="Ativo"
         />
         <div>
           <Button {...buttonProps}>Salvar</Button>
